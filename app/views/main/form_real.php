5841ca4c5e8a6e9eb299a89d111505487557ab6f
Cristian <pavelacu@gmail.com>
2018-06-08 15:15:43 -0600

    se modifica tabla de bitacora de puntos

--- app/views/main/form_real.php
+++ app/views/main/form_real.php
@@ -6,11 +6,11 @@
     <span style="text-height: 30px"><?= $ronda ?></span><br>
     <span style="text-height: 30px">Fecha actual: <b><?= $fecha ?></b></span>
     <form name="form1" id="form1">
-        <table align="center" class="zebra" border="0" style="text-align: center; width: 450px;" cellspacing="1" cellpadding="1" >
+        <table align="center" class="zebra" border="1" style="text-align: center; width: 600px;" cellspacing="1" cellpadding="1" >
 
             <tr>
-                <th colspan="7"> Expectativa</th>                
-                <th colspan="7" style="text-align: right;"> Realidad</th>
+                <th > Expectativa</th>                
+                <th  > Realidad</th>
                 <th> Puntos </th>
             </tr>
             <?php
@@ -31,28 +31,29 @@
 
                 <?php
                 if ($fecha2 != $fechatemp) {
-                    echo "<tr><td colspan='15'><p><b>Jornada: $fecha2<b></p></td></tr>";
+                    echo "<tr><td colspan='3'><p><b>Jornada: $fecha2<b></p></td></tr>";
                     $fechatemp = $fecha2;
                 }
                 ?>
 
 
                 <tr>
-                    <td style="text-align: right"><?= utf8_encode($nombre1) ?></td>
-                    <td><img src="<?= Front::myUrl('images/band/' . $bandera1) ?>"></td>                    
-                    <td><?= $row->marcador1 ?></td>
-                    <td><b>Vs</b></td>
-                    <td><?= $row->marcador2 ?></td>                    
-                    <td><img src="<?= Front::myUrl('images/band/' . $bandera2) ?>"></td>
-                    <td style="text-align: left"><?= utf8_encode($nombre2) ?></td>                    
-
-                    <td style="text-align: right"><?= utf8_encode($nombre1) ?></td>
-                    <td><img src="<?= Front::myUrl('images/band/' . $bandera1) ?>"></td>
-                    <td><?= $row->real1 ?></td>                    
-                    <td><b>Vs</b></td>                    
-                    <td><?= $row->real2 ?></td>
-                    <td><img src="<?= Front::myUrl('images/band/' . $bandera2) ?>"></td>
-                    <td style="text-align: left"><?= utf8_encode($nombre2) ?></td>
+                    <td ><?= utf8_encode($nombre1) ?>
+                    <img src="<?= Front::myUrl('images/band/' . $bandera1) ?>">
+                    <?= $row->marcador1 ?>
+                    <b>-</b>
+                    <?= $row->marcador2 ?>
+                    <img src="<?= Front::myUrl('images/band/' . $bandera2) ?>">
+                    <?= utf8_encode($nombre2) ?>                   
+                                        </td>
+                    <td ><?= utf8_encode($nombre1) ?>
+                    <img src="<?= Front::myUrl('images/band/' . $bandera1) ?>">
+                    <?= $row->real1 ?>
+                    <b>-</b>                
+                    <?= $row->real2 ?>
+                    <img src="<?= Front::myUrl('images/band/' . $bandera2) ?>">
+                    <?= utf8_encode($nombre2) ?>
+                    </td>
 
                     <td><b><?= $row->puntaje ?></b></td>
                 </tr>
