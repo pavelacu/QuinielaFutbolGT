2d9e07271ac9f3789872aa44c69bf98c8b9f05d7
Cristian <pavelacu@gmail.com>
2018-06-11 15:42:14 -0600

    Elimina regla de una hora antes

--- app/views/play/rules.php
+++ app/views/play/rules.php
@@ -5,9 +5,7 @@
                  src="<?= Front::myUrl('images/zabivaka_full.png') ?>"></h1>
         </div>
         <div style="width: 80%; padding: 30px">
-            <ul>
-                <li>Para participar debe llenar la quiniela al menos 1 hora antes de cada juego (no es necesario
-                        llenarla completa de una vez)</li>
+            <ul>                
                 <li>Los puntos son: (5) acertar resultado y marcador exacto, (3) acertar al resultado</li>
                 <li>Se premiaran 1ero(70%) , 2do(20%) y 3er Lugar(10%). (% De lo recaudado)</li>
                 <li>En caso de empate en punteo, se considera la cantidad de aciertos exactos. </li>
